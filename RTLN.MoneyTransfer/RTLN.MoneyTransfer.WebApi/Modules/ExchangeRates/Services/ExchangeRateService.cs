a0b90e0ec3672d1648fbd0d1bc5869a6eca7d5b0
islommakhsudov <islommahsudov20@gmail.com>
2023-02-03 09:34:31 +0500

    Update ExchangeRateService.cs

--- RTLN.MoneyTransfer/RTLN.MoneyTransfer.WebApi/Modules/ExchangeRates/Services/ExchangeRateService.cs
+++ RTLN.MoneyTransfer/RTLN.MoneyTransfer.WebApi/Modules/ExchangeRates/Services/ExchangeRateService.cs
@@ -33,30 +33,7 @@ namespace RTLN.MoneyTransfer.WebApi.Modules.ExchangeRates.Services
 
         public string GetExchangeRatesTest()
         {
-            return GetJsonString();
-        }
-
-        private string GetJsonString()
-        {
-            string jsonText = "{\"effectiveDate\": \"2020-01-01T08:00:00Z\",\"exchangeRatesList\":[{\"currencyCode\": \"TJS\",\"sellRate\": 0.1519,\"buyRate\": 0.1533,\"participantId\": 10003},{\"currencyCode\": \"TJS\",\"sellRate\": 0.1518,\"buyRate\": 0.1534,\"participantId\": 10004}]}";
-           /* string jsonTextSystem = "{\"EffectiveDate\":\"2023-01-05T11:42:39.7700041+05:00\",\"ExchangeRatesList\":[{\"CurrencyCode\":\"1\",\"SellRate\":0,\"BuyRate\":0,\"ParticipantId\":1},{\"CurrencyCode\":\"2\",\"SellRate\":0,\"BuyRate\":0,\"ParticipantId\":2}]}";
-            var exchangeRates = new ExchangeRateModelsResponse();
-            exchangeRates.EffectiveDate = DateTime.Now;
-
-            var exchangeRateList = new List<ExchangeRate>
-            {
-                new ExchangeRate{BuyRate = 0, SellRate = 0, CurrencyCode = "1", ParticipantId = 1},
-                new ExchangeRate{BuyRate = 0, SellRate = 0, CurrencyCode = "2", ParticipantId = 2}
-            };
-
-            exchangeRates.ExchangeRatesList = exchangeRateList;
-
-            string json = JsonSerializer.Serialize(exchangeRates);
-
-            var exchangeRatesNew = JsonSerializer.Deserialize<ExchangeRateModelsResponse>(json);*/
-
-            return jsonText;
-
+            return "{\"effectiveDate\": \"2020-01-01T08:00:00Z\",\"exchangeRatesList\":[{\"currencyCode\": \"TJS\",\"sellRate\": 0.1519,\"buyRate\": 0.1533,\"participantId\": 10003},{\"currencyCode\": \"TJS\",\"sellRate\": 0.1518,\"buyRate\": 0.1534,\"participantId\": 10004}]}";
         }
     }
 }
